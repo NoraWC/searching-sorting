28adf135435d8951a178b6c744b906f574580a37
Nora Coates <noracoates@students.berkeley.net>
2018-04-16 11:33:59 -0700

    search+sort

--- Deck.java
+++ Deck.java
@@ -102,12 +102,63 @@ public class Deck {
         }
     }
     public void mergeSortHand() {
-        //split array into 2 and keep going until you have to stop
-        //sort each part of the array
-        //merge them into one
-        Card[] halfHand1 = new Card[(hand.length+1)/2];
-        System.arraycopy(hand, 0, halfHand1, halfHand1.length, hand.length/2);
+        //keep halving array until you get to 1s
+        //sort each of the arrays + merge into pairs, 4s, etc
+
+
+        //need to make sure the arrays will split down all the way
+        //how to swap the arrays?
+
+        Card[] sorted = new Card[(hand.length)];
+
+        for(int i = 0; i < hand.length; i ++) {
+            //splits array for 1st time (halves)
+            Card[] halfHand1 = new Card[(hand.length)/2];
+            Card[] halfHand2 = new Card[(hand.length)/2];
+            System.arraycopy(hand, 0, halfHand1, 0, halfHand1.length);
+            System.arraycopy(hand, hand.length/2, halfHand2, 0, halfHand2.length);
+
+            //splits array again (quarters)
+            Card[] tempArray = new Card[(halfHand1.length)/2];
+            Card[] tempArray2 = new Card[(halfHand1.length)/2];
+            Card[] tempArray3 = new Card[(halfHand2.length)/2];
+            Card[] tempArray4 = new Card[(halfHand2.length)/2];
+
+            System.arraycopy(halfHand1, 0, tempArray, 0, tempArray.length);
+            System.arraycopy(halfHand1, halfHand1.length/2, tempArray2, 0, tempArray.length);
+
+            System.arraycopy(halfHand2, 0, tempArray3, 0, tempArray.length);
+            System.arraycopy(halfHand2, halfHand2.length/2, tempArray4, 0, tempArray.length);
+
+
+            //swaps within the quarters
+            for(int x = 0; x < halfHand1.length-1; x++) {
+                if(tempArray[x].value > tempArray[x+1].value) {
+                    swap(tempArray, x, x+1);
+                }
+                if(tempArray2[x].value > tempArray2[x+1].value) {
+                    swap(tempArray2, x, x+1);
+                }
+
+                if(tempArray3[x].value > tempArray3[x+1].value) {
+                    swap(tempArray3, x, x+1);
+                }
+                if(tempArray4[x].value > tempArray4[x+1].value) {
+                    swap(tempArray4, x, x+1);
+                }
+
+                System.arraycopy(tempArray, 0, halfHand1, 0, halfHand1.length);
+                System.arraycopy(tempArray2, halfHand1.length/2, halfHand1, 0, halfHand1.length);
+
+                System.arraycopy(tempArray3, 0, halfHand2, 0, halfHand2.length);
+                System.arraycopy(tempArray4, halfHand2.length/2, halfHand2, 0, halfHand2.length);
+            }
+            System.arraycopy(halfHand1, 0, hand, 0, hand.length);
+            System.arraycopy(halfHand2, hand.length/2, hand, 0, hand.length);
+        }
+
     }
+
     public void binarySearch(int suit, int face) {
 
     }
